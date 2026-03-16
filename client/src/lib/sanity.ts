7ebe52610ded20bcda1ec426334ff44b6a357c46
Gaurav <your-email@gmail.com>
2026-03-16 16:27:22 +0530

    fix: disable sanity cdn to bypass cors cache issue

--- client/src/lib/sanity.ts
+++ client/src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 export const sanityClient = createClient({
     projectId: "7fwra7kt",
     dataset: "production", // default dataset
-    useCdn: true, // `false` if you want to ensure fresh data
+    useCdn: false, // Disabling CDN to bypass possible CORS cache issue
     apiVersion: "2024-02-24", // use current date
 });
 
